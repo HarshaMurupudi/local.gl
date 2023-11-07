492aeeb0a9fd3ee9c5c50f53856c958d579590b4
Harsha Murupudi <itsmeeeharsha@gmail.com>
2023-11-07 09:11:17 -0600

    FIX: Space in opening fodlers

--- src/routes/parts.js
+++ src/routes/parts.js
@@ -1,35 +1,35 @@
-const express = require("express");
-const fs = require("fs"); // Or `import fs from "fs";` with ESM
+const express = require('express');
+const fs = require('fs'); // Or `import fs from "fs";` with ESM
 
 const router = express.Router();
 
-router.get("/folders/parts/:partID", async (req, res) => {
+router.get('/folders/parts/:partID', async (req, res) => {
   try {
     const { partID } = req.params;
-    var isWin = process.platform === "win32";
+    var isWin = process.platform === 'win32';
 
     const filePath = isWin
       ? `\\\\gl-fs01\\GLIParts\\${partID}\\`
       : `/Volumes/GLIParts/${partID}/`;
-    const execPath = isWin ? `start "" "${filePath}"` : `open ${filePath}`;
+    const execPath = isWin ? `start "" "${filePath}"` : `open "${filePath}"`;
 
     if (fs.existsSync(filePath)) {
       // Do something
-      await require("child_process").exec(execPath);
+      await require('child_process').exec(execPath);
 
       res.status(200).json({
-        status: "success",
+        status: 'success',
       });
     } else {
       res.status(400).json({
-        status: "Error",
-        message: "No folder",
+        status: 'Error',
+        message: 'No folder',
       });
     }
   } catch (error) {
     console.log(error);
     res.status(400).json({
-      status: "Error",
+      status: 'Error',
       message: error.message,
       code: error.code,
     });
